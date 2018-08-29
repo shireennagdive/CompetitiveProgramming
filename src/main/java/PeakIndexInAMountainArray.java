0f7858523e7fa52e1d8c0158bf61a43273fcd39b
shireennagdive <shireen.nagdive54@gmail.com>
2018-08-29 19:24:23 -0400

    Solved in O(Log(n)) -- Binary Search

--- src/main/java/PeakIndexInAMountainArray.java
+++ src/main/java/PeakIndexInAMountainArray.java
@@ -6,16 +6,13 @@ public class PeakIndexInAMountainArray {
     }
 
     public static int peakIndexInMountainArray(int[] A) {
-        int max, low = 0, high = A.length - 1, mid = 0;
-        while (low <= high) {
+        int low = 0, high = A.length - 1, mid = 0;
+        while (low < high) {
             mid = (low + high) / 2;
-            max = A[mid];
-            if (mid != high && max < A[mid + 1]) {
+            if (A[mid] < A[mid + 1]) {
                 low = mid + 1;
-            } else if (max < A[mid - 1]) {
-                high = mid - 1;
             } else {
-                return mid;
+                high = mid;
             }
         }
         return mid;
