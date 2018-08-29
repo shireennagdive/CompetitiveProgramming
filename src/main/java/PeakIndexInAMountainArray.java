1bfbee493aa169c06fb152e239efddd65c346b01
shireennagdive <shireen.nagdive54@gmail.com>
2018-08-29 14:51:39 -0400

    Solved in O(Log(n)) -- Binary Search

--- src/main/java/PeakIndexInAMountainArray.java
+++ src/main/java/PeakIndexInAMountainArray.java
@@ -1,16 +1,24 @@
 public class PeakIndexInAMountainArray {
 
     public static void main(String[] args) {
-        int A[] = {1,2,3,4,1000000,4,3,2,1};
+        int A[] = {24,69,100,99,79,78,67,36,26,19};
         System.out.println(peakIndexInMountainArray(A));
     }
 
     public static int peakIndexInMountainArray(int[] A) {
-        for(int i =0;i<A.length-1;i++){
-            if(A[i] > A[i+1]){
-                return i;
+        int max, low = 0, high = A.length - 1, mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            max = A[mid];
+            if (mid != high && max < A[mid + 1]) {
+                low = mid + 1;
+            } else if (max < A[mid - 1]) {
+                high = mid - 1;
+            } else {
+                return mid;
             }
         }
-        return 0;
+        return mid;
     }
+
 }
