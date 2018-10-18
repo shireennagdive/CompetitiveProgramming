86b4be6bbc3d7d3ca0afa9d140b4403674a3cf34
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-10-18 01:45:49 -0400

    Problem 21

--- src/main/java/MergeSortedLists.java
+++ src/main/java/MergeSortedLists.java
@@ -11,30 +11,18 @@ public class MergeSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(-1);
-        head.next = null;
-        ListNode l3 = head;
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                l3.next = l1;
-                l1 = l1.next;
-            } else {
-                l3.next = l2;
-                l2 = l2.next;
-            }
-            l3 = l3.next;
+        if (l1 == null) {
+            return l2;
         }
-        while (l1 != null) {
-            l3.next = l1;
-            l1 = l1.next;
-            l3 = l3.next;
+        if (l2 == null) {
+            return l1;
         }
-        while (l2 != null) {
-            l3.next = l2;
-            l2 = l2.next;
-            l3 = l3.next;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
         }
-        return head.next;
     }
-
 }
