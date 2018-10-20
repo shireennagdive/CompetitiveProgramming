752b42403dfac8e6e786189edc593f579354ed9c
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-10-20 16:52:53 -0400

    Problem 415

---
+++ src/main/java/AddStrings.java
@@ -0,0 +1,14 @@
+class AddStrings {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int carry=0;
+        for(int i=num1.length()-1, j=num2.length()-1; i>=0 || j>=0 || carry==1;i--,j--){
+            int digit1 = i<0 ? 0 : num1.charAt(i)-'0';
+            int digit2 = j<0 ? 0 : num2.charAt(j)-'0';
+            result.append(( digit1+ digit2 + carry)%10);
+            carry=(digit1+ digit2 + carry)/10;
+        }
+        return result.reverse().toString();
+    }
+
+}
