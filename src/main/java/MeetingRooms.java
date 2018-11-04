4f0bb0fea3e180ff9f8ab5f572abf9b23f8f1ad6
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-11-03 20:00:46 -0400

    Problem 252

---
+++ src/main/java/MeetingRooms.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Interval {
+      int start;
+      int end;
+      Interval() { start = 0; end = 0; }
+      Interval(int s, int e) { start = s; end = e; }
+  }
+
+public class MeetingRooms {
+
+        public boolean canAttendMeetings(Interval[] intervals) {
+            Arrays.sort(intervals, new Comparator<Interval>() {
+                @Override
+                public int compare(Interval o1, Interval o2) {
+                    return o1.start - o2.start;
+                }});
+
+            for(int i=1;i<intervals.length;i++){
+                if(intervals[i].start < intervals[i-1].end){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+}
