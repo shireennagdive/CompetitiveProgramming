f67b0990a7806cb042d297cbe0c275559ea51f26
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-11-19 21:13:29 -0500

    Problem 844

--- src/main/java/BackSpaceStringCompare.java
+++ src/main/java/BackSpaceStringCompare.java
@@ -1,31 +1,22 @@
 public class BackSpaceStringCompare {
-    public boolean backspaceCompare(String S, String T) {
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
-        int i,count=0;
-        char ch;
-        for(i=S.length()-1;i>=0;i--){
-            ch=S.charAt(i);
-            if(ch == '#'){
-                count++;
-            }else if(count==0){
-                s1.append(ch);
-            }else if(count>0){
-                count--;
-            }
+        public boolean backspaceCompare(String S, String T) {
+            return removeSpaces(S).toString().equals(removeSpaces(T).toString());
         }
-        count=0;
-        for(i=T.length()-1;i>=0;i--){
-            ch=T.charAt(i);
-            if(ch == '#'){
-                count++;
-            }else if(count==0){
-                s2.append(ch);
-            }else if(count>0){
-                count--;
+        public static StringBuilder removeSpaces(String S){
+            int count=0;
+            char ch;
+            StringBuilder s1 = new StringBuilder();
+            for(int i=S.length()-1;i>=0;i--){
+                ch=S.charAt(i);
+                if(ch == '#'){
+                    count++;
+                }else if(count==0){
+                    s1.append(ch);
+                }else if(count>0){
+                    count--;
+                }
             }
+            return s1;
         }
 
-        return s1.toString().equals(s2.toString());
     }
-}
