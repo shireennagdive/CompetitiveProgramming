beb55faa45657485e0e446c8df4d466e12a832b9
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-11-24 12:53:50 -0500

    Problem 26

--- src/main/java/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/RemoveDuplicatesFromSortedArray.java
@@ -15,4 +15,18 @@ public class RemoveDuplicatesFromSortedArray {
             //Arrays.copyOfRAnge(nums,0,k)
             return i;
         }
+
+        /*
+        Cleaner code
+        public int removeDuplicates(int[] nums) {
+        if(nums.length==0) return 0;
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j-1]!=nums[j]){
+               nums[i]  = nums[j];
+               i++;
+            }
+        }
+        return i;
+    }*/
     }
