9b4d0aca174b837b4f734f1040fe96734e3b06eb
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-11-27 10:51:29 -0500

    Problem 438 - Acute must read solution

--- src/main/java/FindAllAnagrams.java
+++ src/main/java/FindAllAnagrams.java
@@ -2,27 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindAllAnagrams {
+//
         public List<Integer> findAnagrams(String s, String p) {
             List<Integer> res = new ArrayList<Integer>();
             if(s.equals("") || p.length() > s.length() ) return res;
             int[] pattern = new int[26];
             int len = p.length(),i;
-            int front=0,end=len-1;
+            int diff=p.length(); //unseen
+            int front=0,end=0;
+            char temp;
             for(char c : p.toCharArray()){
                 pattern[c-'a']++;
             }
-            for(i=front;i<=end;i++){
-                pattern[s.charAt(i)-'a']--;
+            while(end<p.length()){
+                temp = s.charAt(end);
+                pattern[temp-'a']--;
+                if(pattern[temp-'a'] >= 0) diff--;
+                end++;
             }
+            if(diff==0) res.add(0);
             while(end<s.length()){
-                for(i=front;i<=end;i++) {
-                    if(pattern[s.charAt(i) - 'a']!=0) break;
-                }
-                if(i==end+1) res.add(front);
-                pattern[s.charAt(front) - 'a']++;
-                front++;end++;
-                if(end!=s.length())pattern[s.charAt(end) - 'a']--;
+                temp = s.charAt(front);
+                if(pattern[temp-'a']>=0) diff++;
+                pattern[temp-'a']++;
+                front++;
+                temp = s.charAt(end);
+                pattern[temp-'a']--;
+                if(pattern[temp-'a']>=0) diff--;
+                if(diff==0) res.add(front);
+                end++;
             }
             return res;
         }
     }
+
