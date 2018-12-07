6b595285cf3118f81b383f83623c5082836bd171
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-12-07 11:32:42 -0500

    Problem 716 - Better Approach

--- src/main/java/MaxStack.java
+++ src/main/java/MaxStack.java
@@ -1,90 +1,39 @@
-public class MaxStack {
-    /**
-     * initialize your data structure here.
-     */
-    class Node {
-        int val;
-        Node next;
+import java.util.Stack;
 
-        Node(int val) {
-            this.val = val;
-            this.next = null;
-        }
-    }
-
-    Node head = null;
-    int max = Integer.MIN_VALUE;
+class MaxStack {
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
 
     public MaxStack() {
+        stack = new Stack();
+        maxStack = new Stack();
     }
 
     public void push(int x) {
-        Node node = new Node(x);
-        node.next = head;
-        head = node;
-        if (max == Integer.MIN_VALUE) {
-            max = head.val;
-        } else if (node.val > max) {
-            max = node.val;
-        }
+        int max = maxStack.isEmpty() ? x : maxStack.peek();
+        maxStack.push(max > x ? max : x);
+        stack.push(x);
     }
 
     public int pop() {
-        if (head == null) return -1;
-        int val = head.val;
-        head = head.next;
-        max = Integer.MIN_VALUE;
-        Node temp = head;
-        while (temp != null) {
-            if (temp.val > max) {
-                max = temp.val;
-            }
-            temp = temp.next;
-        }
-        return val;
+        maxStack.pop();
+        return stack.pop();
     }
 
     public int top() {
-        return head.val;
+        return stack.peek();
     }
 
     public int peekMax() {
-        return max;
+        return maxStack.peek();
     }
 
     public int popMax() {
-        Node prev = null;
-        Node curr = head;
-        int val = max;
-        Node temp = head;
-        if (head.val == max) {
-            head = head.next;
-        } else {
-            while (curr.val != max) {
-                prev = curr;
-                curr = curr.next;
-            }
-            prev.next = curr.next;
-        }
-        max = Integer.MIN_VALUE;
-        temp = head;
-        while (temp != null) {
-            if (temp.val > max) {
-                max = temp.val;
-            }
-            temp = temp.next;
-        }
-        return val;
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack();
+        while (top() != max) buffer.push(pop());
+        pop();
+        while (!buffer.isEmpty()) push(buffer.pop());
+        return max;
     }
 }
-
-/**
- * Your MaxStack object will be instantiated and called as such:
- * MaxStack obj = new MaxStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.peekMax();
- * int param_5 = obj.popMax();
- */
-
