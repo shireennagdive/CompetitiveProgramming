502e4b5b07f8ca5693a0491a2dc4f4c89ea08f24
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-12-25 21:15:03 -0500

    Problem 278

---
+++ src/main/java/FirstBadVersion.java
@@ -0,0 +1,19 @@
+public class FirstBadVersion {
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+        public int firstBadVersion(int n) {
+
+            int low=1,high=n,mid;
+            while(low<high){
+                mid = low + (high-low)/2;
+                if(isBadVersion(mid)){
+                    high=mid;
+                }else{
+                    low=mid+1;
+                }
+            }
+            return high;
+        }
+    }
+
