46bff64710ed6bc7a57cacd45d90aca99d3eb5a9
shireennagdive <shireen.nagdive@stonybrook.edu>
2018-12-27 00:52:59 -0500

    Problem 53

--- src/main/java/MaximumSubArray.java
+++ src/main/java/MaximumSubArray.java
@@ -4,26 +4,34 @@ public class MaximumSubArray {
 
 
     public static void main(String[] args) {
-        int input[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(maxSubArray(input));
+        String s = "The      girl is      beauty";
+        for(String temp : s.split(" ")){
+            System.out.println(temp);
+        }
     }
 
-    public static int maxSubArray(int[] nums) {
-        int length = nums.length;
-        int[] sum = new int[length];
-        for (int i = 0; i < length; i++) {
-            sum[i] = nums[i];
-        }
-        int maxSum = sum[0];
-        for (int i = 0; i < length - 1; i++) {
-            if (sum[i] + nums[i + 1] > sum[i + 1]) {
-                sum[i + 1] = sum[i] + nums[i + 1];
+        public static int maxSubArray(int[] nums) {
+            int maxSum = Integer.MIN_VALUE;
+            int[] sums = new int[nums.length];
+            sums[0]=nums[0];
+            int max=nums[0];
+            for(int i=1;i<nums.length;i++){
+                if(nums[i]+sums[i-1] > nums[i]){
+                    sums[i] = nums[i]+sums[i-1];
+                }else{
+                    sums[i] = nums[i];
+                }
+                if(sums[i] > max){
+                    max = sums[i];
+                }
+                //System.out.println(sums[i]);
             }
-            if (sum[i + 1] > maxSum) {
-                maxSum = sum[i + 1];
-            }
+            return max;
         }
-        return length == 1 ? sum[0] : maxSum;
+
+    {
+
     }
-}
+    }
+
 
