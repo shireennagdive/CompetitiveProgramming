8689f61d96ac7a294a415b2bef3e2222306b89ed
shireennagdive <shireen.nagdive@stonybrook.edu>
2019-01-01 20:09:40 -0500

    PROBLEM 325

---
+++ src/main/java/MaximumSizeSubArray.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumSizeSubArray {
+        public int maxSubArrayLen(int[] nums, int k) {
+            if(nums.length==0) return 0;
+            Map<Integer,Integer> map = new HashMap<>();
+            map.put(0,-1);
+            int sum=0,max=0;
+            for(int i=0;i<nums.length;i++){
+                sum+=nums[i];
+                if(map.containsKey(sum-k)){
+                    max=Math.max(i-map.get(sum-k),max);
+                }
+                if(!map.containsKey(sum)){
+                    map.put(sum,i);
+                }
+            }
+            return max;
+        }
+}
