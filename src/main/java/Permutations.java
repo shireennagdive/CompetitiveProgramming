d5b41ffddce7ffd58428d3d57d535b1f39cf8389
shireennagdive <shireen.nagdive@stonybrook.edu>
2019-01-06 18:20:24 -0500

    PROBLEM 46

--- src/main/java/Permutations.java
+++ src/main/java/Permutations.java
@@ -1,29 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Permutations {
-        List<List<Integer>> res;
-        public List<List<Integer>> permute(int[] nums) {
-            res = new ArrayList<>();
-            List<Integer> list = new ArrayList<>();
-            dfs(nums, list);
-            return res;
+class Permutations {
+    List<List<Integer>> res;
+    boolean[] visited;
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        visited = new boolean[nums.length];
+        dfs(nums, list);
+        return res;
+    }
+
+    public void dfs(int[] nums, List<Integer> list){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<>(list));
+            return;
         }
 
-        public void dfs(int[] nums, List<Integer> list){
-            if(list.size()==nums.length){
-                res.add(new ArrayList<>(list));
-                return;
-            }
-
-            for(int i=0;i<nums.length;i++){
-                if(!list.contains(nums[i])){
-                    list.add(nums[i]);
-                    System.out.println(nums[i]);
-                    dfs(nums, list);
-                    list.remove(list.size()-1);
-                }
-
+        for(int i=0;i<nums.length;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                list.add(nums[i]);
+                dfs(nums, list);
+                list.remove(list.size()-1);
+                visited[i]= false;
             }
         }
     }
+}
