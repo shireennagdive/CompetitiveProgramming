294500892e85d09e48e395a318d3bb9c8998c676
shireennagdive <shireen.nagdive@stonybrook.edu>
2019-01-13 12:52:29 -0500

    PROBLEM 31

--- src/main/java/NextPermutation.java
+++ src/main/java/NextPermutation.java
@@ -1,39 +1,35 @@
-public class NextPermutation {
-        public void nextPermutation(int[] nums) {
-            int pivot=-1, nextMax=nums.length-1;
-            for(int i=nums.length-1;i>=1;i--){
-                if(nums[i-1]<nums[i]){
-                    pivot = i-1;
-                    break;
-                }
-            }
+class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int pivot, nextMax=nums.length-1;
+        int i=nums.length-1;
+        while(i>=1 && nums[i-1]>=nums[i]) i--;
 
-            if(pivot==-1){
-                reverse(nums,0);
-                return;
-            }
+        pivot=i-1;
 
-            while(nextMax > pivot){
-                if(nums[nextMax] > nums[pivot]){
-                    break;
-                }
-                nextMax--;
-            }
-
-            swap(nums, pivot, nextMax);
-            reverse(nums, pivot+1);
+        if(i==-1){
+            reverse(nums,0,nums.length-1);
+            return;
         }
 
-        public void swap(int[] nums, int i, int j){
-            int temp = nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        while(nums[nextMax]<=nums[pivot]){
+            nextMax--;
         }
 
-        public void reverse(int nums[], int start){
-            int end = nums.length;
-            for(int i=start;i<(end+start)/2;i++){
-                swap(nums, i, end-i-1 + start);
-            }
+        swap(nums, pivot, nextMax);
+        reverse(nums, pivot+1,nums.length-1);
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    public void reverse(int nums[], int start, int end){
+        while(start<end){
+            swap(nums, start, end);
+            start++;
+            end--;
         }
+    }
 }
